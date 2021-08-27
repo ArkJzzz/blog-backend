11c81d05ec0bdd28272132709ff95e009d772d06
ArkJzzz <arkjzzz@gmail.com>
2021-08-27 19:35:32 +0300

    Add index page layout

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -94,7 +94,10 @@ USE_TZ = True
 
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "templates"),
+    os.path.join(BASE_DIR, "../olya-frontend/"),
+)
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
